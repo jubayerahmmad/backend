adf0dadf7a807c08a089180e9a578eac6ddc8eee
jubayerahmmad <zubayersami510@gmail.com>
2025-05-19 10:33:32 +0600

    updated cors origin

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 app.use(express.json());
 
 // Routes
